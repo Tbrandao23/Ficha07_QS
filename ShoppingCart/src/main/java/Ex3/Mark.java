dd415313633ef80d1c7c883613e8112b55aab99a
Tbrandao23 <tiagoleal76@gmail.com>
2023-10-20 10:31:20 +0100

    Ficha 07 v2

---
+++ ShoppingCart/src/main/java/Ex3/Mark.java
@@ -0,0 +1,32 @@
+package Ex3;
+
+public class Mark {
+    int numStudent;
+    double mark;
+
+    public Mark(int numStudent, double mark) {
+        this.numStudent = numStudent;
+        this.mark = mark;
+    }
+
+    public int getNumStudent() {
+        return numStudent;
+    }
+
+    public void setNumStudent(int numStudent) {
+        this.numStudent = numStudent;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+
+    public void insertMarkCU(int numStudent, double mark){
+
+    }
+
+}
---
+++ ShoppingCart/src/main/java/Ex3/UC.java
@@ -0,0 +1,16 @@
+package Ex3;
+
+public class UC {
+    String nameCu;
+    Mark[] marks;
+    int totalStudents;
+
+    public UC(String nameCu, Mark[] marks, int totalStudents) {
+        this.nameCu = nameCu;
+        this.marks = marks;
+        this.totalStudents = totalStudents;
+    }
+
+
+
+}
